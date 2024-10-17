b8a9eec58d502352404fcc00937997faea92d862
s2358034 <s.p.g.nijhuis@student.utwente.nl>
2024-10-17 15:24:27 +0200

    feat: test optimized next/image imports

--- components/vosteq/experts.tsx
+++ components/vosteq/experts.tsx
@@ -10,6 +10,14 @@ import {
 } from "@/components/ui/carousel"
 import { Icon } from "@iconify/react";
 import Image from "next/image";
+import Harry from "@/public/images/experts/Harry.webp";
+import Ronald from "@/public/images/experts/Ronald_de_Witte.webp";
+import Maarten from "@/public/images/experts/Maarten_Woertman.webp";
+import Marijke from "@/public/images/experts/Marijke_Oosterveld.webp";
+import Stijn from "@/public/images/experts/Stijn_Nijhuis.webp";
+import Bob from "@/public/images/experts/Bob_Selker.webp";
+import Joyce from "@/public/images/experts/Joyce_Kloosterman.webp";
+
 
 const Experts = () => {
   const [api, setApi] = useState<CarouselApi>()
@@ -23,49 +31,49 @@ const Experts = () => {
       name: "Harry",
       role: "Directeur Vosteq",
       description: "Directeur Vosteq, werktuigbouwkundige, met passie voor het innoveren en automatiseren van processen Betrokken en gericht op het samen ontdekken en implementeren van resultaat.",
-      imageSrc: "/images/experts/Harry.webp"
+      imageSrc: Harry
     },
     {
       key: 2,
       name: "Ronald",
       role: "Directeur Vosteq",
       description: "Directeur Vosteq, werktuigbouwkundige, met passie voor het innoveren en automatiseren van processen Betrokken en gericht op het samen ontdekken en implementeren van resultaat.",
-      imageSrc: "/images/experts/Ronald_de_Witte.webp"
+      imageSrc: Ronald
     },
     {
       key: 3,
       name: "Maarten",
       role: "Directeur Vosteq",
       description: "Directeur Vosteq, werktuigbouwkundige, met passie voor het innoveren en automatiseren van processen Betrokken en gericht op het samen ontdekken en implementeren van resultaat.",
-      imageSrc: "/images/experts/Maarten_Woertman.webp"
+      imageSrc: Maarten
     },
     {
       key: 4,
       name: "Marijke",
       role: "Directeur Vosteq",
       description: "Directeur Vosteq, werktuigbouwkundige, met passie voor het innoveren en automatiseren van processen Betrokken en gericht op het samen ontdekken en implementeren van resultaat.",
-      imageSrc: "/images/experts/Marijke_Oosterveld.webp"
+      imageSrc: Marijke
     },
     {
       key: 5,
       name: "Stijn",
       role: "Directeur Vosteq",
       description: "Directeur Vosteq, werktuigbouwkundige, met passie voor het innoveren en automatiseren van processen Betrokken en gericht op het samen ontdekken en implementeren van resultaat.",
-      imageSrc: "/images/experts/Stijn_Nijhuis.webp"
+      imageSrc: Stijn
     },
     {
       key: 6,
       name: "Bob",
       role: "Directeur Vosteq",
       description: "Directeur Vosteq, werktuigbouwkundige, met passie voor het innoveren en automatiseren van processen Betrokken en gericht op het samen ontdekken en implementeren van resultaat.",
-      imageSrc: "/images/experts/Bob_Selker.webp"
+      imageSrc: Bob
     },
     {
       key: 7,
       name: "Joyce",
       role: "Directeur Vosteq",
       description: "Directeur Vosteq, werktuigbouwkundige, met passie voor het innoveren en automatiseren van processen Betrokken en gericht op het samen ontdekken en implementeren van resultaat.",
-      imageSrc: "/images/experts/Joyce_Kloosterman.webp"
+      imageSrc: Joyce
     }
   ]
 
@@ -103,7 +111,7 @@ const Experts = () => {
             <CarouselItem key={item.key} className="basis-2/3 md:basis-1/4 flex flex-col relative">
             <div className="relative group">
               <div className="relative overflow-hidden">
-                <Image className="w-full h-full" src={item.imageSrc} alt={item.name} width={300} height={600} />
+                <Image className="w-full h-full" src={item.imageSrc} alt={item.name} />
             
                 {/* Overlay */}
                 <div
--- components/vosteq/home-hero.tsx
+++ components/vosteq/home-hero.tsx
@@ -3,6 +3,7 @@ import { Link } from "@/lib/i18n";
 import { Separator } from "@/components/ui/separator";
 import { Icon } from "@iconify/react";
 import * as m from "@/paraglide/messages.js";
+import hero from '@/public/images/Vosteq-Hapert-2018-1.webp';
 
 
 const HomeHero = () => {
@@ -17,7 +18,7 @@ const HomeHero = () => {
           <button className='btn-solid'><Link href='/'>{m.cool_noisy_snail_drum()}</Link></button>
           <button className='btn-solid'><Link href='/'>{m.blue_warm_bee_dig()}</Link></button>
         </div>
-        <Image className='md:-mt-48 object-cover md:translate-y-1/2 max-md:pt-16' src="/images/Vosteq-Hapert-2018-1.webp" alt='Hero' height={400} width={1100} />
+        <Image className='md:-mt-48 object-cover md:translate-y-1/2 max-md:pt-16' src={hero} alt='Vosteq bij Hapert' placeholder="blur" />
         <div className="w-40 bg-geel p-6 flex flex-col gap-2 text-paars absolute right-8 top-1/2 group hover:bg-groen hover:text-wit transition-all duration-200 max-md:hidden">
           <p className="font-aktiv-grotesk-extended text-lg leading-5 font-bold">{m.patchy_plane_lark_breathe()}</p>
           <Separator className="bg-paars group-hover:bg-wit transition-all duration-200"/>
