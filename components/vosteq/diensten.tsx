9b447065fdb8b3e82ace17d5d7e0ce0e583fb756
s2358034 <s.p.g.nijhuis@student.utwente.nl>
2024-10-31 13:43:56 +0100

    chore: update diensten design

--- components/vosteq/diensten.tsx
+++ components/vosteq/diensten.tsx
@@ -74,37 +74,42 @@ const Diensten = () => {
           </div>
         </div>
       </div>
-      <div className='container flex flex-col md:flex-row py-16'>
-        <div className='md:w-1/2 flex flex-col gap-8 justify-center'>
-          <h2 className='text-paars max-md:text-2xl'>Hoe gaan we te werk?</h2>
-          <Image className='object-cover py-4 max-md:hidden' src="/images/systeem.svg" alt='Het systeem van Vosteq' height={80} width={370} />
+      <div className='bg-gradient-to-b from-paars to-groen from-30%'>
+        <div className='container flex flex-col md:flex-row py-16'>
+          <div className='md:w-1/2 flex flex-col gap-8 justify-center'>
+            <h2 className='text-wit max-md:text-2xl'>Hoe gaan we te werk?</h2>
+            <Image className='object-cover py-4 max-md:hidden' src="/images/systeem.svg" alt='Het systeem van Vosteq' height={80} width={370} />
+          </div>
+          <Accordion type='single' collapsible className='md:w-1/2'>
+            <AccordionItem value='stap1' className='my-2'>
+              <AccordionTrigger className='text-wit text-sm md:text-lg'><div className='flex'><p className='pr-2 font-bold'>Stap 1.</p>Inzicht krijgen in je bedrijf</div></AccordionTrigger>
+              <AccordionContent className='md:text-base text-wit my-4'>
+                Wat is de huidige staat van jouw organisatie? Om een duidelijk antwoord op deze vraag te krijgen, organiseren we koffiegesprekken en analyseren we de bedrijfsprocessen en -technologieën, de organisatiestructuur, de leiderschapscapaciteiten en de werknemersbetrokkenheid.
+              </AccordionContent>
+            </AccordionItem>
+            <Separator className='bg-wit' />
+            <AccordionItem value='stap2' className='my-2'>
+              <AccordionTrigger className='text-wit text-sm md:text-lg'><div className='flex'><p className='pr-2 font-bold'>Stap 2.</p>Identificeren van kernkwaliteiten</div></AccordionTrigger>
+              <AccordionContent className='md:text-base text-wit my-4'>
+                We helpen je bij het identificeren van de unieke kwaliteiten en sterke punten binnen jouw organisatie, zoals bijvoorbeeld de innovatieve productieprocessen, het sterke team of de diepgaande technische expertise. Of een combinatie daarvan.
+              </AccordionContent>
+            </AccordionItem>
+            <Separator className='bg-wit' />
+            <AccordionItem value='stap3' className='my-2'>
+              <AccordionTrigger className='text-wit text-sm md:text-lg'><div className='flex'><p className='pr-2 font-bold'>Stap 3.</p>Strategische versterking</div></AccordionTrigger>
+              <AccordionContent className='md:text-base text-wit my-4'>
+                Op basis van onze inzichten ontwikkelen we strategieën om de kernkwaliteiten verder te versterken. Dit kan variëren van procesoptimalisatie, automatisering, digitalisering en/of organisatieontwikkeling tot het verbeteren van de bedrijfscultuur en het innovatievermogen.
+              </AccordionContent>
+              <Separator className='bg-wit' />
+              </AccordionItem>
+            <AccordionItem value='stap4' className='my-2'>
+              <AccordionTrigger className='text-wit text-sm md:text-lg'><div className='flex'><p className='pr-2 font-bold'>Stap 4.</p>Implementatie en begeleiding</div></AccordionTrigger>
+              <AccordionContent className='md:text-base text-wit my-4'>
+                We begeleiden je bij de implementatie van deze strategieën en zorgen ervoor dat ze in de dagelijkse operaties van je bedrijf worden geïntegreerd.
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
         </div>
-        <Accordion type='single' collapsible className='md:w-1/2'>
-          <AccordionItem value='stap1'>
-            <AccordionTrigger className='text-paars text-sm md:text-lg'><div className='flex'><p className='pr-2 font-bold'>Stap 1.</p>Inzicht krijgen in je bedrijf</div></AccordionTrigger>
-            <AccordionContent className='md:text-base'>
-              Wat is de huidige staat van jouw organisatie? Om een duidelijk antwoord op deze vraag te krijgen, organiseren we koffiegesprekken en analyseren we de bedrijfsprocessen en -technologieën, de organisatiestructuur, de leiderschapscapaciteiten en de werknemersbetrokkenheid.
-            </AccordionContent>
-          </AccordionItem>
-          <AccordionItem value='stap2'>
-            <AccordionTrigger className='text-paars text-sm md:text-lg'><div className='flex'><p className='pr-2 font-bold'>Stap 2.</p>Identificeren van kernkwaliteiten</div></AccordionTrigger>
-            <AccordionContent className='md:text-base'>
-              We helpen je bij het identificeren van de unieke kwaliteiten en sterke punten binnen jouw organisatie, zoals bijvoorbeeld de innovatieve productieprocessen, het sterke team of de diepgaande technische expertise. Of een combinatie daarvan.
-            </AccordionContent>
-          </AccordionItem>
-          <AccordionItem value='stap3'>
-            <AccordionTrigger className='text-paars text-sm md:text-lg'><div className='flex'><p className='pr-2 font-bold'>Stap 3.</p>Strategische versterking</div></AccordionTrigger>
-            <AccordionContent className='md:text-base'>
-              Op basis van onze inzichten ontwikkelen we strategieën om de kernkwaliteiten verder te versterken. Dit kan variëren van procesoptimalisatie, automatisering, digitalisering en/of organisatieontwikkeling tot het verbeteren van de bedrijfscultuur en het innovatievermogen.
-            </AccordionContent>
-          </AccordionItem>
-          <AccordionItem value='stap4'>
-            <AccordionTrigger className='text-paars text-sm md:text-lg'><div className='flex'><p className='pr-2 font-bold'>Stap 4.</p>Implementatie en begeleiding</div></AccordionTrigger>
-            <AccordionContent className='md:text-base'>
-              We begeleiden je bij de implementatie van deze strategieën en zorgen ervoor dat ze in de dagelijkse operaties van je bedrijf worden geïntegreerd.
-            </AccordionContent>
-          </AccordionItem>
-        </Accordion>
       </div>
       <div className='md:container'>
         <div className="container flex flex-col items-center md:py-16 max-md:pb-16">
@@ -131,15 +136,14 @@ const Diensten = () => {
           </div>
         </div>
       </div>
-      <div className='bg-[#F1F9F9]'>
-        <div className='flex md:container flex-col md:flex-row gap-16 md:gap-32 items-center'>
-          <div className='md:w-1/2 max-md:pt-16 max-md:container'>
-            <h2 className='text-paars'>Waarom kiezen voor Vosteq?</h2>
-            <p className='text-diepgrijs font-aptos pt-4'>Vosteq geeft al meer dan 25 jaar advies aan MKB-maakbedrijven. Wij begrijpen dat elke organisatie uniek is en daarom ontwikkelen we maatwerkstrategieën die aansluiten bij de specifieke behoeften en doelen van jouw organisatie. Lees hier meer over in <Link className='text-donkergroen underline hover:text-paars' href={'/'}>onze cases</Link> of <Link className='text-donkergroen underline hover:text-paars' href={'/contact'}>nodig ons uit</Link> voor een kopje koffie.</p>
-          </div>
-          <div className='md:w-1/2 p-12 md:p-24 h-full bg-gradient-to-br from-paars to-groen from-30%'>
-            <p className='pb-4 text-wit max-md:text-sm font-aktiv-grotesk-extended'>Wil je succesvolle groei en duurzame winstgevendheid voor jouw organisatie realiseren? Neem dan vandaag nog contact met ons op en ontdek hoe ons directieadvies jouw organisatie van binnenuit versterkt. </p>
-            <button className='btn-solid'><Link href='/contact'>Contact</Link></button>
+      <div className='bg-paars w-full'>
+        <div className='container md:pl-32 py-20'>
+          <div className='w-full md:w-1/2 flex flex-col gap-8'>
+            <h2 className='text-wit'>Wil je succesvolle groei en duurzame winstgevendheid?</h2>
+            <p className='text-wit'>Neem dan vandaag nog contact met ons op en ontdek hoe ons directieadvies jouw organisatie van binnenuit versterkt.</p>
+            <div>
+              <button className='btn-solid'>Neem contact op</button>
+            </div>
           </div>
         </div>
       </div>
