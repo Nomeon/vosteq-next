411c79383618016741ce7e622eac439e623091b3
s2358034 <s.p.g.nijhuis@student.utwente.nl>
2024-11-29 11:00:19 +0100

    feat: diensten pages done, except for expertise-on-demand

--- app/diensten/innovatie-en-verandertrajecten/page.tsx
+++ app/diensten/innovatie-en-verandertrajecten/page.tsx
@@ -1,37 +1,41 @@
-import Image from 'next/image';
 import { Popup } from '@/components/ui/popup';
 import type { Metadata } from "next";
-import { WatLevertHetOp } from "@/components/vosteq/wat-levert-het-op";
+import { WatLevertHetOp } from '@/components/vosteq/wat-levert-het-op';
 import { WerkStappen } from '@/components/vosteq/werk-stappen';
 import { BottomCta } from '@/components/vosteq/bottom-cta';
 import DecoRight from '@/components/vosteq/deco-right';
 
 export const metadata: Metadata = {
-  title: "Vosteq | Innovatie- en verandertrajecten",
-  description: "Aan het bouwen!",
+  title: "Innovatie- en Verandertrajecten voor MKB-Maakbedrijven | Vosteq ",
+  description: "Vosteq helpt jouw MKB-maakbedrijf met innovatie- en verandertrajecten. Voer transformaties door zonder je operaties te verstoren. Lees hoe ons parttime innovatieteam werkt!",
 };
 
 export default function InnovatieEnVerandertrajecten() {
   const steps = [
     {
       icon: 'mdi:cog-transfer',
-      title: 'Verbeterde interne efficiëntie',
-      description: 'door processen te optimaliseren en middelen beter in te zetten.'
-    },
-    {
-      icon: 'mdi:account-group',
-      title: 'Een sterkere bedrijfscultuur',
-      description: 'waar medewerkers zich betrokken voelen en bijdragen aan het succes van het bedrijf.'
+      title: 'Soepel lopende innovaties',
+      description: 'en veranderingen zonder verstoringen in de dagelijkse processen.'
     },
     {
       icon: 'mdi:shield-check',
-      title: 'Een duurzaam concurrentievoordeel',
-      description: 'door te bouwen op de unieke interne kernwaarden binnen jouw organisatie.'
+      title: 'Duurzame resultaten',
+      description: 'omdat we pas vertrekken wanneer de innovatie effectief is geïmplementeerd in jouw organisatie.'
+    },
+    {
+      icon: 'mdi:account-group',
+      title: 'Expertise op maat',
+      description: 'met kennis en ervaring uit verschillende vakgebieden dat de last van projectwerkzaamheden op zich neemt.'
     },
     {
       icon: 'mdi:chart-line',
-      title: 'Betere besluitvorming',
-      description: 'dankzij een scherp inzicht in je eigen organisatie en de verbetermogelijkheden.'
+      title: 'Succesvolle transformatie',
+      description: 'die volledig is afgestemd op de behoeften van jouw bedrijf en die meetbare resultaten oplevert.'
+    },
+    {
+      icon: 'mdi:cash-multiple',
+      title: 'Lage kosten',
+      description: 'door efficiënt gebruik te maken van de parttime inzet van onze specialisten, zonder onnodige overhead.'
     }
   ];
 
@@ -39,26 +43,26 @@ export default function InnovatieEnVerandertrajecten() {
     {
       value: 'stap1',
       stap: 'Stap 1.',
-      title: 'Inzicht krijgen in je bedrijf',
-      description: 'Wat is de huidige staat van jouw organisatie? Om een duidelijk antwoord op deze vraag te krijgen, organiseren we koffiegesprekken en analyseren we de bedrijfsprocessen en -technologieën, de organisatiestructuur, de leiderschapscapaciteiten en de werknemersbetrokkenheid.'
+      title: 'Analyse en planning',
+      description: 'We starten met een grondige analyse van jouw organisatie en het geplande project. Samen ontwikkelen we een gedetailleerd plan van aanpak dat rekening houdt met jouw operationele processen én de gewenste innovaties.'
     },
     {
       value: 'stap2',
       stap: 'Stap 2.',
-      title: 'Identificeren van kernkwaliteiten',
-      description: 'We helpen je bij het identificeren van de unieke kwaliteiten en sterke punten binnen jouw organisatie, zoals bijvoorbeeld de innovatieve productieprocessen, het sterke team of de diepgaande technische expertise. Of een combinatie daarvan.'
+      title: 'Gerichte ondersteuning',
+      description: 'Op basis van jouw specifieke behoeften stellen we een op maat gemaakt team samen. Van het coördineren van projectactiviteiten tot het tijdelijk versterken van je team met gespecialiseerde kennis, wij staan klaar. En afhankelijk van de voortgang en complexiteit van het project passen we onze inzet flexibel aan.'
     },
     {
       value: 'stap3',
       stap: 'Stap 3.',
-      title: 'Strategische versterking',
-      description: 'Op basis van onze inzichten ontwikkelen we strategieën om de kernkwaliteiten verder te versterken. Dit kan variëren van procesoptimalisatie, automatisering, digitalisering en/of organisatieontwikkeling tot het verbeteren van de bedrijfscultuur en het innovatievermogen.'
+      title: 'Naadloze implementatie',
+      description: 'Ons team werkt nauw samen met jouw medewerkers om te zorgen voor een soepele uitvoering van het project. Hierbij zorgen we dat innovaties niet alleen worden ingevoerd, maar ook direct werken binnen jouw organisatie.'
     },
     {
       value: 'stap4',
       stap: 'Stap 4.',
-      title: 'Implementatie en begeleiding',
-      description: 'We begeleiden je bij de implementatie van deze strategieën en zorgen ervoor dat ze in de dagelijkse operaties van je bedrijf worden geïntegreerd.'
+      title: 'Capaciteitsmanagement',
+      description: 'We monitoren voortdurend de capaciteit van jouw team en passen onze aanpak aan om ervoor te zorgen dat de dagelijkse operaties probleemloos blijven verlopen.'
     }
   ]
 
@@ -67,19 +71,19 @@ export default function InnovatieEnVerandertrajecten() {
       <Popup />
       <div className="container flex flex-col md:flex-row gap-16 md:gap-32 md:py-16 py-4">
         <div className="md:w-1/2 flex flex-col gap-4">
-          <p className='font-aktiv-grotesk-extended text-base text-donkergroen'>Organisatie verbetering</p>
-          <h1 className="text-paars">Directie advies</h1>
-          <p className="font-aktiv-grotesk-extended text-paars">Succesvolle groei en duurzame winstgevendheid beginnen bij een solide interne basis. Bij Vosteq bieden we directieadvies dat zich richt op het versterken van jouw organisatie van binnenuit, zodat je beter voorbereid bent om externe uitdagingen aan te gaan en kansen te benutten. Hierbij nemen we jouw organisatie en haar onderscheidende krachten als uitgangspunt.</p>
-          <h2 className='pt-8 text-paars max-md:text-2xl'>Diepgaande analyse</h2>
-          <p className='text-diepgrijs'>Onze "inside-out benadering" begint met een diepgaande analyse van de interne processen, de bedrijfscultuur en de middelen in jouw organisatie. In plaats van veel nadruk te leggen op externe trends en bedreigingen, richten we ons eerst op wat jouw bedrijf uniek maakt en hoe je deze interne krachten kunt versterken om zo een concurrentievoordeel te creëren.</p>
+          <p className='font-aktiv-grotesk-extended text-base text-donkergroen'>Verandering zonder verstoring</p>
+          <h1 className="text-paars">Innovatie- en verandertrajecten op maat</h1>
+          <p className="font-aktiv-grotesk-extended text-paars">In de maakindustrie is het vinden van balans tussen dagelijkse operaties en innovatie cruciaal. Bij Vosteq begrijpen we dat verandertrajecten en nieuwe projecten een druk kunnen leggen op jouw team en processen. Daarom bieden we kosteneffectieve, parttime innovatie- en verbetertrajecten die jouw organisatie helpen groeien, zonder de kernactiviteiten te verstoren.</p>
+          <h3 className='pt-8 text-paars max-md:text-2xl'>Een parttime innovatieteam voor maximale impact tegen minimale kosten</h3>
+          <p className='text-diepgrijs'>Bij complexe transformaties is een geïntegreerde aanpak essentieel. Ons parttime innovatieteam bestaat uit directieadviseurs, projectmanagers, interim-managers en specialistische experts. Samen leveren we de kennis en kunde die nodig zijn om jouw doelen te realiseren – zonder de kosten van fulltime rollen.</p>
         </div>
         <div className="md:w-1/2 container relative pb-16">
-          <DecoRight imageSrc="/images/directieadvies.webp" />
+          <DecoRight imageSrc='/images/projectmanagement.webp' />
         </div>
       </div>
       <WerkStappen accordionItems={accordionItems} />
-      <WatLevertHetOp omschrijving='Dankzij onze inside-out benadering realiseren we:' steps={steps} />
-      <BottomCta titel="Wil je succesvolle groei en duurzame winstgevendheid?" paragraph="Neem dan vandaag nog contact met ons op en ontdek hoe ons directieadvies jouw organisatie van binnenuit versterkt." />
+      <WatLevertHetOp omschrijving='Met onze aanpak profiteer je van:' steps={steps} />
+      <BottomCta titel='Wil je transformaties doorvoeren zonder je operaties te verstoren?' paragraph='Ontdek hoe ons parttime innovatieteam jouw bedrijf helpt bij een kosteneffectieve transformatie.' />
     </div>
   );
 }
