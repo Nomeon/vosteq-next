85361d3230efbbf26cde77380da0c937c024931f
Stijn Nijhuis <spg.nijhuis@hotmail.com>
2024-12-05 22:34:00 +0100

    feat: update experts caroussel with new texts

--- lib/carousel-items.ts
+++ lib/carousel-items.ts
@@ -1,46 +1,77 @@
 import Bob from "@/public/images/experts/Bob_Selker.webp";
-import Harry from "@/public/images/experts/Harry_van_de_Vosse.webp";
-import Ronald from "@/public/images/experts/Ronald_de_Witte.webp";
-import Maarten_Woertman from "@/public/images/experts/Maarten_Woertman.webp";
-import Marijke from "@/public/images/experts/Marijke_Oosterveld.webp";
-import Stijn from "@/public/images/experts/Stijn_Nijhuis.webp";
-import Joyce from "@/public/images/experts/Joyce_Kloosterman.webp";
+import Bram from '@/public/images/experts/Bram_van_der_Most.webp';
 import Christiaan from "@/public/images/experts/Christiaan_Kruims.webp";
 import Daniel from "@/public/images/experts/Daniel_Kiemeneij.webp";
 import Frank from "@/public/images/experts/Frank_van_Dijk.webp";
+import Harry from "@/public/images/experts/Harry_van_de_Vosse.webp";
 import Henno from "@/public/images/experts/Henno_Staal.webp";
+// Herman van Ommen
 import Ingrid from "@/public/images/experts/Ingrid_Genger.webp";
+// Jan-Willem Olde Kalter
 import Joost from "@/public/images/experts/Joost_ter_Doest.webp";
+import Joyce from "@/public/images/experts/Joyce_Kloosterman.webp";
 import Maarten_Wijnheijmer from "@/public/images/experts/Maarten_Wijnheijmer.webp";
+import Maarten_Woertman from "@/public/images/experts/Maarten_Woertman.webp";
+import Marijke from "@/public/images/experts/Marijke_Oosterveld.webp";
 import Patrick from "@/public/images/experts/Patrick_Overmaat.webp";
 import Ramon from "@/public/images/experts/Ramon_Huiskamp.webp";
 import Rens from "@/public/images/experts/Rens_Hofstede.webp";
+// Rob ter Meulen
+import Ronald from "@/public/images/experts/Ronald_de_Witte.webp";
 import Ruud from "@/public/images/experts/Ruud_Pullen.webp";
 import Sibbele from "@/public/images/experts/Sibbele_Douma.webp";
+import Stijn from "@/public/images/experts/Stijn_Nijhuis.webp";
 import Tom from "@/public/images/experts/Tom_Hassing.webp";
 import Wessel from "@/public/images/experts/Wessel_van_Zwolle.webp";
 import Wijbrand from '@/public/images/experts/Wijbrand_Medendorp.webp';
+// Willem Lenselink
+// Wim Brink
 
 export const carouselItems = [
     {
       name: "Harry van de Vosse",
-      description: "Directeur Vosteq, werktuigbouwkundige, met passie voor het innoveren en automatiseren van processen Betrokken en gericht op het samen ontdekken en implementeren van resultaat.",
+      description: "Betrokken directieadviseur en werktuigbouwkundige met passie voor innovatie, automatisering en robotisering. Graag realiseer ik meerwaarde en duurzaam resultaat, samen met ondernemers en onze teams.",
       imageSrc: Harry
     },
     // {
-    //   name: "Ronald de Witte",
-    //   description: "",
-    //   imageSrc: Ronald
+    //   name: "Willem Lenselink",
+    //   description: "Betrokken en resultaatgerichte directieadviseur, gespecialiseerd in procesoptimalisatie en organisatieontwikkeling. Samen met ondernemers en het team doe ik wat nodig is voor duurzame oplossingen en strategische verbeteringen.",
+    //   imagesrc: Willem
     // },
+    {
+      name: "Ronald de Witte",
+      description: "Van nature een optimistisch en resultaatgericht persoon. Ik weet snel en effectief te schakelen bij veranderende omstandigheden en combineer dit met een sterk creatief probleemoplossend vermogen.",
+      imageSrc: Ronald
+    },
     // {
     //   name: "Maarten Woertman",
     //   description: "",
     //   imageSrc: Maarten_Woertman
     // },
     {
+      name: "Christiaan Kruims",
+      description: "Als adviseur weet ik, geen bedrijf is hetzelfde. Mijn missie is, samen met directie en medewerkers te zoeken naar de beste organisatie inpassing, die aansluit bij de ontwikkeling van het bedrijf.",
+      imageSrc: Christiaan
+    },
+    {
       name: "Marijke Oosterveld",
       description: "Ik help bedrijven veranderingen en innovaties succesvol te integreren in hun dagelijkse praktijk. Als interimmanager of procesbegeleider inspireer ik teams om actief en enthousiast bij te dragen aan de transformatie",
       imageSrc: Marijke
+    },
+    {
+      name: "Bram van der Most",
+      description: "Als project- en verandermanager met een achtergrond in engineering en technisch bedrijfskunde begrijp ik de uitdagingen van maakbedrijven. Ik help hen complexe projecten succesvol en duurzaam te realiseren.",
+      imageSrc: Bram
+    },
+    {
+      name: "Joost ter Doest",
+      description: "Vanuit de rol van interim directeur structureer ik in korte tijd de organisatie met als doel de performance van het MKB maakbedrijf te verbeteren en optimaliseren.",
+      imageSrc: Joost
+    },
+    {
+      name: "Ingrid Genger",
+      description: "Met mijn jarenlange ervaring als secretaresse ontzorg ik met heel veel plezier de directie en het bestuur. Door te doen waar ik goed in ben, hebben zij de tijd om te excelleren. Teamwork!",
+      imageSrc: Ingrid
     },
     {
       name: "Stijn Nijhuis",
@@ -52,11 +83,6 @@ export const carouselItems = [
     //   description: "",
     //   imageSrc: Joyce
     // },
-    {
-      name: "Christiaan Kruims",
-      description: "Als adviseur weet ik, geen bedrijf is hetzelfde. Mijn missie is, samen met directie en medewerkers te zoeken naar de beste organisatie inpassing, die aansluit bij de ontwikkeling van het bedrijf.",
-      imageSrc: Christiaan
-    },
     // {
     //   name: "Daniel Kiemeneij",
     //   description: "",
@@ -67,25 +93,15 @@ export const carouselItems = [
     //   description: "",
     //   imageSrc: Frank
     // },
-    // {
-    //   name: "Bob Selker",
-    //   description: "",
-    //   imageSrc: Bob
-    // },
+    {
+      name: "Bob Selker",
+      description: "Proces engineer die productieprocessen optimaliseert en innoveert, met aandacht voor opleiding, begeleiding en draagvlak. Met een toekomstgerichte aanpak ondersteun ik technische en organisatorische transities.",
+      imageSrc: Bob
+    },
     {
       name: "Henno Staal",
       description: "Ondersteunt maakbedrijven als adviseur, coach, projectmanager en lean expert. Voor nieuwe productieconcepten in Operations en Supply Chain waarbij verbetering en verandering hand in hand gaan.",
       imageSrc: Henno
-    },
-    {
-      name: "Ingrid Genger",
-      description: "Met mijn jarenlange ervaring als secretaresse ontzorg ik met heel veel plezier de directie en het bestuur. Door te doen waar ik goed in ben, hebben zij de tijd om te excelleren. Teamwork!",
-      imageSrc: Ingrid
-    },
-    {
-      name: "Joost ter Doest",
-      description: "Vanuit de rol van interim directeur structureer ik in korte tijd de organisatie met als doel de performance van het MKB maakbedrijf te verbeteren en optimaliseren.",
-      imageSrc: Joost
     },
     {
       name: "Maarten Wijnheijmer",
@@ -98,6 +114,11 @@ export const carouselItems = [
     //   imageSrc: Patrick
     // },
     {
+      name: "Ruud Pullen",
+      description: "Met visie en als generalist, ervaringsdeskundige en rustgevend klankbord voor directies, plaatsen we samen de stip-op-de-horizon en pakken we door op de verdere organisatieontwikkeling. Lijden wordt leiden.",
+      imageSrc: Ruud
+    },
+    {
       name: "Ramon Huiskamp",
       description: "Ervaren projectleider, projectmanager voor productontwikkeling, productieautomatisering en/of speciaal machinebouw. Expert in het leiden van technische projecten en het implementeren van innovatieve oplossingen.",
       imageSrc: Ramon
@@ -107,29 +128,24 @@ export const carouselItems = [
       description: "Met 40 jaar ervaring help ik ondernemers met procesverbetering, ERP-optimalisatie en maatwerkoplossingen. Samen realiseren we duurzame groei, meer efficiëntie en een toekomstbestendige organisatie.",
       imageSrc: Rens
     },
-    {
-      name: "Ruud Pullen",
-      description: "Met visie en als generalist, ervaringsdeskundige en rustgevend klankbord voor directies, plaatsen we samen de stip-op-de-horizon en pakken we door op de verdere organisatieontwikkeling. Lijden wordt leiden.",
-      imageSrc: Ruud
-    },
     // {
     //   name: "Sibbele Douma",
     //   description: "",
     //   imageSrc: Sibbele
     // },
     {
+      name: "Wijbrand Medendorp",
+      description: "Creatieve trouble-shooter met oog voor mens en machine. Ik verbeter bedrijfsresultaten duurzaam door voordoen, samen doen, laten doen en los hardnekkige problemen structureel op met statistische meet en analyse techniek.",
+      imageSrc: Wijbrand
+    },
+    {
       name: "Tom Hassing",
       description: "Ervaren hands-on C-level manager die duurzaam rendement zal realiseren. Brengt inzicht, inspiratie, motivatie en plezier. Zorgt met passie voor visie, strategie, structuur en flow. Verbetert dagelijks op alle niveaus.",
       imageSrc: Tom
-    },
+    }
     // {
     //   name: "Wessel van Zwolle",
     //   description: "",
     //   imageSrc: Wessel
     // },
-    {
-      name: "Wijbrand Medendorp",
-      description: "Creatieve trouble-shooter met oog voor mens en machine. Ik verbeter bedrijfsresultaten duurzaam door voordoen, samen doen, laten doen en los hardnekkige problemen structureel op met statistische meet en analyse techniek.",
-      imageSrc: Wijbrand
-    }
 ]
