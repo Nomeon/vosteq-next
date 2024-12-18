82bcc2c2e3aa077a0653cfcba3194e064bce08c9
Stijn Nijhuis <spg.nijhuis@hotmail.com>
2024-12-18 12:12:06 +0100

    chore: remove vercel speedinsights, disable indexing for staging website

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,13 +4,16 @@ import { LanguageProvider } from "@inlang/paraglide-next"
 import { languageTag } from "@/paraglide/runtime.js"
 import { Header } from "@/components/nav/header";
 import { Footer } from "@/components/nav/footer";
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { ReactNode } from 'react';
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export const metadata = {
+  robots: {
+    index: false,
+    follow: false,
+  }
+}
+
+export default function RootLayout({ children }: Readonly<{ children: ReactNode }>) {
   return (
     <LanguageProvider>
       <html lang={languageTag()}> 
@@ -18,7 +21,6 @@ export default function RootLayout({
           <Header />
           <main>
             {children}
-            <SpeedInsights />
           </main>
           <Footer />
         </body>
