ed823d3e84bd6c90987b2bd7a6001053403e9e0d
Stijn Nijhuis <spg.nijhuis@hotmail.com>
2025-01-26 18:56:40 +0100

    feat: add indexing

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,13 +6,6 @@ import { Header } from "@/components/nav/header";
 import { Footer } from "@/components/nav/footer";
 import { ReactNode } from 'react';
 
-export const metadata = {
-  robots: {
-    index: false,
-    follow: false,
-  }
-}
-
 export default function RootLayout({ children }: Readonly<{ children: ReactNode }>) {
   return (
     <LanguageProvider>
