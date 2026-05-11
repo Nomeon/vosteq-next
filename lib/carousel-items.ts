909de1c04bb3885248900123c247831a3453da97
Stijn Nijhuis <spg.nijhuis@hotmail.com>
2026-05-11 21:51:38 +0200

    feat: add Roland, remove Christiaan

--- lib/carousel-items.ts
+++ lib/carousel-items.ts
@@ -1,12 +1,10 @@
 import Alice from "@/public/images/experts/Alice_Spijker.webp";
 import Bob from "@/public/images/experts/Bob_Selker.webp";
 import Bram from "@/public/images/experts/Bram_van_der_Most.webp";
-import Christiaan from "@/public/images/experts/Christiaan_Kruims.webp";
 import Daniel from "@/public/images/experts/Daniel_Kiemeneij.webp";
 import Harry from "@/public/images/experts/Harry_van_de_Vosse.webp";
 import Henno from "@/public/images/experts/Henno_Staal.webp";
 import Ingrid from "@/public/images/experts/Ingrid_Genger.webp";
-// Jan-Willem Olde Kalter
 import Joost from "@/public/images/experts/Joost_ter_Doest.webp";
 import Joyce from "@/public/images/experts/Joyce_Kloosterman.webp";
 import Lars from "@/public/images/experts/Lars_Theunissen.webp";
@@ -25,145 +23,139 @@ import Wijbrand from "@/public/images/experts/Wijbrand_Medendorp.webp";
 import Willem from "@/public/images/experts/Willem_Lenselink.webp";
 import Herman from "@/public/images/experts/Herman_van_Ommen.webp";
 import Arjan from "@/public/images/experts/Arjan_Ester.webp";
-// import Roland from "@/public/images/experts/Roland_Bosman.webp";
+import Roland from "@/public/images/experts/Roland_Bosman.webp";
 
 export const carouselItems = [
   {
     name: "Harry van de Vosse",
     description:
-    "Betrokken directieadviseur en werktuigbouwkundige met passie voor innovatie, automatisering en robotisering. Graag realiseer ik meerwaarde en duurzaam resultaat, samen met ondernemers en onze teams.",
+      "Betrokken directieadviseur en werktuigbouwkundige met passie voor innovatie, automatisering en robotisering. Graag realiseer ik meerwaarde en duurzaam resultaat, samen met ondernemers en onze teams.",
     imageSrc: Harry,
   },
   {
     name: "Willem Lenselink",
     description:
-    "Betrokken en resultaatgerichte directieadviseur, gespecialiseerd in procesoptimalisatie en organisatieontwikkeling. Samen met ondernemers en het team doe ik wat nodig is voor duurzame oplossingen en strategische verbeteringen.",
+      "Betrokken en resultaatgerichte directieadviseur, gespecialiseerd in procesoptimalisatie en organisatieontwikkeling. Samen met ondernemers en het team doe ik wat nodig is voor duurzame oplossingen en strategische verbeteringen.",
     imageSrc: Willem,
   },
   {
     name: "Ronald de Witte",
     description:
-    "Van nature een optimistisch en resultaatgericht persoon. Ik weet snel en effectief te schakelen bij veranderende omstandigheden en combineer dit met een sterk creatief probleemoplossend vermogen.",
+      "Van nature een optimistisch en resultaatgericht persoon. Ik weet snel en effectief te schakelen bij veranderende omstandigheden en combineer dit met een sterk creatief probleemoplossend vermogen.",
     imageSrc: Ronald,
   },
   {
     name: "Maarten Woertman",
     description:
-    "Bedrijfseconoom en business architect met bijna 30 jaar ervaring in de maakindustrie. Creëert strategische verbeteringen en zorgt voor pragmatische oplossingen en optimalisatie van Productie en Logistiek.",
+      "Bedrijfseconoom en business architect met bijna 30 jaar ervaring in de maakindustrie. Creëert strategische verbeteringen en zorgt voor pragmatische oplossingen en optimalisatie van Productie en Logistiek.",
     imageSrc: Maarten_Woertman,
-  },
-  {
-    name: "Christiaan Kruims",
-    description:
-    "Als adviseur weet ik, geen bedrijf is hetzelfde. Mijn missie is, samen met directie en medewerkers te zoeken naar de beste organisatie inpassing, die aansluit bij de ontwikkeling van het bedrijf.",
-    imageSrc: Christiaan,
   },
   {
     name: "Marijke Oosterveld",
     description:
-    "Ik help bedrijven veranderingen en innovaties succesvol te integreren in hun dagelijkse praktijk. Als interimmanager of procesbegeleider inspireer ik teams om actief en enthousiast bij te dragen aan de transformatie",
+      "Ik help bedrijven veranderingen en innovaties succesvol te integreren in hun dagelijkse praktijk. Als interimmanager of procesbegeleider inspireer ik teams om actief en enthousiast bij te dragen aan de transformatie",
     imageSrc: Marijke,
   },
   {
     name: "Bram van der Most",
     description:
       "Als project- en verandermanager met een achtergrond in engineering en technisch bedrijfskunde begrijp ik de uitdagingen van maakbedrijven. Ik help hen complexe projecten succesvol en duurzaam te realiseren.",
-      imageSrc: Bram,
-    },
-    {
-      name: "Joost ter Doest",
-      description:
+    imageSrc: Bram,
+  },
+  {
+    name: "Joost ter Doest",
+    description:
       "Vanuit de rol van interim directeur structureer ik in korte tijd de organisatie met als doel de performance van het MKB maakbedrijf te verbeteren en optimaliseren.",
-      imageSrc: Joost,
-    },
-    {
-      name: "Ingrid Genger",
-      description:
+    imageSrc: Joost,
+  },
+  {
+    name: "Ingrid Genger",
+    description:
       "Met mijn jarenlange ervaring als secretaresse ontzorg ik met heel veel plezier de directie en het bestuur. Door te doen waar ik goed in ben, hebben zij de tijd om te excelleren. Teamwork!",
-      imageSrc: Ingrid,
-    },
-    {
-      name: "Stijn Nijhuis",
-      description:
+    imageSrc: Ingrid,
+  },
+  {
+    name: "Stijn Nijhuis",
+    description:
       "Als IT-specialist zorg ik voor IT-oplossingen die bedrijfsprocessen versterken. Op het snijvlak tussen bedrijfskunde en informatica vertaal ik de klantvraag naar een passende oplossing.",
     imageSrc: Stijn,
   },
   {
     name: "Joyce Kloosterman",
     description:
-    "Als transitiemanager combineer ik mijn technische achtergrond met mijn passie voor persoonlijke ontwikkeling. Met een creatieve en hands-on aanpak en zet ik in op eigenaarschap, bewustwording, plezier en energie.",
+      "Als transitiemanager combineer ik mijn technische achtergrond met mijn passie voor persoonlijke ontwikkeling. Met een creatieve en hands-on aanpak en zet ik in op eigenaarschap, bewustwording, plezier en energie.",
     imageSrc: Joyce,
   },
   {
     name: "Daniel Kiemeneij",
     description:
-    "Mechanical engineer met passie voor innovatie en expertise in Cad-Cam, engineering en CNC-machines. Met frisse inzichten in smart production bied ik hoogwaardige oplossingen voor complexe technische uitdagingen.",
+      "Mechanical engineer met passie voor innovatie en expertise in Cad-Cam, engineering en CNC-machines. Met frisse inzichten in smart production bied ik hoogwaardige oplossingen voor complexe technische uitdagingen.",
     imageSrc: Daniel,
   },
   {
     name: "Bob Selker",
     description:
-    "Proces engineer die productieprocessen optimaliseert en innoveert, met aandacht voor opleiding, begeleiding en draagvlak. Met een toekomstgerichte aanpak ondersteun ik technische en organisatorische transities.",
+      "Proces engineer die productieprocessen optimaliseert en innoveert, met aandacht voor opleiding, begeleiding en draagvlak. Met een toekomstgerichte aanpak ondersteun ik technische en organisatorische transities.",
     imageSrc: Bob,
   },
   {
     name: "Henno Staal",
     description:
-    "Ondersteunt maakbedrijven als adviseur, coach, projectmanager en lean expert. Voor nieuwe productieconcepten in Operations en Supply Chain waarbij verbetering en verandering hand in hand gaan.",
+      "Ondersteunt maakbedrijven als adviseur, coach, projectmanager en lean expert. Voor nieuwe productieconcepten in Operations en Supply Chain waarbij verbetering en verandering hand in hand gaan.",
     imageSrc: Henno,
   },
   {
     name: "Lars Theunissen",
     description:
-    "Interimmanager als er doorgepakt moet worden. Werktuigbouw- en bedrijfskundige met ruim 10 jaar directie-ervaring in Engineering-to-Order bedrijven. Voor innovatie en groei van MKB maakbedrijven.",
+      "Interimmanager als er doorgepakt moet worden. Werktuigbouw- en bedrijfskundige met ruim 10 jaar directie-ervaring in Engineering-to-Order bedrijven. Voor innovatie en groei van MKB maakbedrijven.",
     imageSrc: Lars,
   },
   {
     name: "Maarten Wijnheijmer",
     description:
-    "Als adviseur en interim-manager help ik bedrijven klantgericht te groeien. Met expertise in strategie, verkoop en service breng ik structuur, focus en resultaat. Ik vertaal visie naar actie.",
+      "Als adviseur en interim-manager help ik bedrijven klantgericht te groeien. Met expertise in strategie, verkoop en service breng ik structuur, focus en resultaat. Ik vertaal visie naar actie.",
     imageSrc: Maarten_Wijnheijmer,
   },
   {
     name: "Patrick Overmaat",
     description:
-    "Als automatiseringsspecialist breng ik mechatronica en bedrijfskunde samen om maatwerkoplossingen te ontwikkelen. Ik optimaliseer productieprocessen gedreven door technische innovatie en klantgerichte samenwerking.",
+      "Als automatiseringsspecialist breng ik mechatronica en bedrijfskunde samen om maatwerkoplossingen te ontwikkelen. Ik optimaliseer productieprocessen gedreven door technische innovatie en klantgerichte samenwerking.",
     imageSrc: Patrick,
   },
   {
     name: "Ramon Huiskamp",
     description:
-    "Ervaren projectleider, projectmanager voor productontwikkeling, productieautomatisering en/of speciaal machinebouw. Expert in het leiden van technische projecten en het implementeren van innovatieve oplossingen.",
+      "Ervaren projectleider, projectmanager voor productontwikkeling, productieautomatisering en/of speciaal machinebouw. Expert in het leiden van technische projecten en het implementeren van innovatieve oplossingen.",
     imageSrc: Ramon,
   },
   {
     name: "Rens Hofstede",
     description:
-    "Met 40 jaar ervaring help ik ondernemers met procesverbetering, ERP-optimalisatie en maatwerkoplossingen. Samen realiseren we duurzame groei, meer efficiëntie en een toekomstbestendige organisatie.",
+      "Met 40 jaar ervaring help ik ondernemers met procesverbetering, ERP-optimalisatie en maatwerkoplossingen. Samen realiseren we duurzame groei, meer efficiëntie en een toekomstbestendige organisatie.",
     imageSrc: Rens,
   },
   {
     name: "Rob ter Meulen",
     description:
-    "Als interimmanager zorg ik voor continuïteit en begeleid ik organisaties bij veranderingen. Met mijn bedrijfskundige expertise verbeter ik processen en structuren. Daarmee verzorg ik een soepele overgang naar een volgende fase.",
+      "Als interimmanager zorg ik voor continuïteit en begeleid ik organisaties bij veranderingen. Met mijn bedrijfskundige expertise verbeter ik processen en structuren. Daarmee verzorg ik een soepele overgang naar een volgende fase.",
     imageSrc: Rob,
   },
   {
     name: "Sibbele Douma",
     description:
-    "Als adviseur en interim-manager creëer ik structuur en verandering om organisaties te versterken. Met mijn bedrijfskundige en technische ervaring vertaal ik strategie naar concrete resultaten op de werkvloer.",
+      "Als adviseur en interim-manager creëer ik structuur en verandering om organisaties te versterken. Met mijn bedrijfskundige en technische ervaring vertaal ik strategie naar concrete resultaten op de werkvloer.",
     imageSrc: Sibbele,
   },
   {
     name: "Wijbrand Medendorp",
     description:
-    "Creatieve trouble-shooter met oog voor mens en machine. Ik verbeter bedrijfsresultaten duurzaam door voordoen, samen doen, laten doen en los hardnekkige problemen structureel op met statistische meet en analyse techniek.",
+      "Creatieve trouble-shooter met oog voor mens en machine. Ik verbeter bedrijfsresultaten duurzaam door voordoen, samen doen, laten doen en los hardnekkige problemen structureel op met statistische meet en analyse techniek.",
     imageSrc: Wijbrand,
   },
   {
     name: "Wessel van Zwolle",
     description:
-    "Mijn passie is de maakindustrie tijdelijk te versterken met leidinggeven, projectmanagement en verbetertrajecten. Altijd hands-on, verbindend, professioneel en mensgericht, met de focus op kansen!",
+      "Mijn passie is de maakindustrie tijdelijk te versterken met leidinggeven, projectmanagement en verbetertrajecten. Altijd hands-on, verbindend, professioneel en mensgericht, met de focus op kansen!",
     imageSrc: Wessel,
   },
   {
@@ -184,10 +176,10 @@ export const carouselItems = [
       "Brengt focus, tempo en scherpte in directieteams. Verbindt strategie met uitvoering en daagt uit om transformatie zichtbaar te maken in resultaat.",
     imageSrc: Arjan,
   },
-  // {
-  //   name: "Roland Bosman",
-  //   description:
-  //     "Mijn kracht zit in het vertalen van inhoud naar communicatie die herkenbaar, onderscheidend en geloofwaardig is. Soms strategisch, soms creatief, soms heel praktisch. Altijd gedreven en betrokken.",
-  //   imageSrc: Roland,
-  // },
+  {
+    name: "Roland Bosman",
+    description:
+      "Mijn kracht zit in het vertalen van inhoud naar communicatie die herkenbaar, onderscheidend en geloofwaardig is. Soms strategisch, soms creatief, soms heel praktisch. Altijd gedreven en betrokken.",
+    imageSrc: Roland,
+  },
 ];
